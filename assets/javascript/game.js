66f3c9fb4d8fed94831a5ffbc510415cfa3594cc
Deion Aujero <deion@aujero.net>
2019-10-07 20:23:20 -0600

    added click functions to pictures

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,25 @@
 
-document.addEventListener("DOMContentLoaded",
-    function () {
-        
-    }
+$(document).ready(function(){
+        var wins = 0;
+        var losses = 0;
+        var totalScore = 0;
+        var computerGuess = Math.floor(Math.random() * 100) + 100;
+        console.log(computerGuess);
+        // $(".crystals").on("click", function() {
+        //     console.log("it works");
+        // });
+// start of functions
+        $("#redCrystal").click(function() {
+        console.log("its working!");
+        });
+        $("#blueCrystal").click(function() {
+        console.log("its working!!");
+        });
+        $("#yellowCrystal").click(function() {
+        console.log("its working!!!");
+        });
+        $("#greenCrystal").click(function() {
+        console.log("its working!!!!");
+        });
+}
 )
